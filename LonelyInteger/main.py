9b8c6accffe48e4f3c9bef0cec6bedc14f2106e1
johnkdunyo <johnkdunyo@gmail.com>
2022-07-18 11:21:42 +0000

    added code lonely integer problem

---
+++ LonelyInteger/main.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    ans = 0
+    for i in set(a):
+        print(i, ' occurs ', a.count(i))
+        if(a.count(i)==1):
+            ans = i
+            break
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
