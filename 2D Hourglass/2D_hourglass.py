dc605ecd0da1835634226463af18425d1c43907c
johnkdunyo <johnkdunyo@gmail.com>
2022-08-05 12:52:41 +0000

    added code for hourglass and repeated string

---
+++ 2D Hourglass/2D_hourglass.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    final_result = []
+    for x in range(len(arr)-2):
+        for y in range(len(arr)-2):
+            final_result.append(
+                arr[x][y]+arr[x][y+1]+arr[x][y+2]+arr[x+1][y+1]+arr[x+2][y]+arr[x+2][y+1]+arr[x+2][y+2]
+                )
+    return max(final_result)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
